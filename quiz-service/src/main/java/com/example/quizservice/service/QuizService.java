d90202874d7729dd8cd23ce6ab66e3c161786e1e
mohamed <m.shah@lamma.om>
2024-05-16 17:14:28 +0530

    first commit

---
+++ quiz-service/src/main/java/com/example/quizservice/service/QuizService.java
@@ -0,0 +1,49 @@
+package com.example.quizservice.service;
+
+import com.example.quizservice.dao.QuizDao;
+import com.example.quizservice.feign.QuizInterface;
+import com.example.quizservice.model.QuestionWrapper;
+import com.example.quizservice.model.Quiz;
+import com.example.quizservice.model.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class QuizService {
+
+    @Autowired
+    QuizDao quizDao;
+
+    @Autowired
+    QuizInterface quizInterface;
+
+    public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
+        // call the generate url - RestTemplate http://localhost:8080/question/generate
+        List<Integer> questions = quizInterface.getQuestionsForQuiz(category, numQ).getBody();
+
+        Quiz quiz = new Quiz();
+        quiz.setTitle(title);
+        quiz.setQuestionIds(questions);
+        quizDao.save(quiz);
+
+        return new ResponseEntity<>("Success", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Quiz quiz = quizDao.findById(id).get();
+        List<Integer> questionIds = quiz.getQuestionIds();
+        ResponseEntity<List<QuestionWrapper>> questions = quizInterface.getQuestionsFromId(questionIds);
+        return questions;
+    }
+
+    public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
+        ResponseEntity<Integer> score = quizInterface.getScore(responses);
+        return score;
+    }
+}
